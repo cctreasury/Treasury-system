d4029ad13c6845241b7eb763cc5e75a75d9a6740
CC Treasury Profile <97610885+CC-Treasury@users.noreply.github.com>
2022-01-15 18:43:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,25 +32,25 @@ ${description}`
     const encodedFileText = encodeURIComponent(fileText)
   
     //Generate a github link with query parameter
-    //const githubQueryLink2 = "https://github.com/Glaxas/CC-treasury/new/main/Transaction%20recordings/AAT/new?value=" + encodedFileText +"&filename=" + filename
+    
     function githubQueryLink(pool) {
       var answer = "";
       switch(pool) {
         case 'Power Up Catalyst Circle':
-          answer = "https://github.com/cctreasury/treasury-system/tree/main/Transaction%20recordings/Fund6/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund6/PowerUpTCC/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         case 'CC Admin Team Scope Expansion':
-          answer = "https://github.com/cctreasury/treasury-system/tree/main/Transaction%20recordings/Fund7/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/CCAdminTSE/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         case 'Sustaining The Circle':
-          answer = "https://github.com/cctreasury/treasury-system/tree/main/Transaction%20recordings/Fund7/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/SustainingTC/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         case 'Treasury Management System':
-          answer = "https://github.com/cctreasury/treasury-system/tree/main/Transaction%20recordings/Fund7/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/TreasuryMS/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         default:
-          answer = "https://github.com/cctreasury/treasury-system/tree/main/Transaction%20recordings/new?value=" + encodedFileText +"&filename=" + filename;
-          beark;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/new?value=" + encodedFileText +"&filename=" + filename;
+          break;
       }
     
       return answer;
