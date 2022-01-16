ecbd769488fa4b671e376341cc00ec45c9698faf
Glaxas <32074058+Glaxas@users.noreply.github.com>
2022-01-16 11:50:42 +0200

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getValue(name){
   function validateSubmission(){
     //save all the input values
     const name = getValue('name')
-    const role = getValue('role')
+    const budgetB = getValue('budgetB')
     const ada = getValue('ada')
     const txid = getValue('txid')
     const description = getValue('description')
@@ -20,13 +20,13 @@ function getValue(name){
     //Indentation is important here
     let fileText = `---
 Date: ${new Date().toUTCString()}
-name: "${name}"
-role: "${role}"
-ada: "${ada}"
-txid: "${txid}"
-pool: "${pool}"
+Name: "${name}"
+Budget Breakdown Item: "${budgetB}"
+ADA: "${ada}"
+Transaction ID: "${txid}"
+Funded Proposal: "${pool}"
 ---
-${description}`
+Description: ${description}`
     
     //Encode string to URI format
     const encodedFileText = encodeURIComponent(fileText)
@@ -37,16 +37,16 @@ ${description}`
       var answer = "";
       switch(pool) {
         case 'Power Up Catalyst Circle':
-          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund6/PowerUpTCC/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund6/Power-Up-The-Catalyst-Circle/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         case 'CC Admin Team Scope Expansion':
-          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/CCAdminTSE/new?value=" + encodedFileText +"&filename=" + filename;
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/CC-Admin-Team-Scope-Expantion/new?value=" + encodedFileText +"&filename=" + filename;
           break;
-        case 'Sustaining The Circle':
-          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/SustainingTC/new?value=" + encodedFileText +"&filename=" + filename;
+        case 'CCv3 Sustaining The Circle':
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/CCv3-Sustaining-the-circle/new?value=" + encodedFileText +"&filename=" + filename;
           break;
-        case 'Treasury Management System':
-          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/TreasuryMS/new?value=" + encodedFileText +"&filename=" + filename;
+        case 'CC Treasury Management':
+          answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/Fund7/CC-Treasury-Management/new?value=" + encodedFileText +"&filename=" + filename;
           break;
         default:
           answer = "https://github.com/cctreasury/treasury-system/new/main/Transaction%20recordings/new?value=" + encodedFileText +"&filename=" + filename;
